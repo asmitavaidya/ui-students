6f67906f0df5ce05ef40a2e46d31076e3add4da4
Asmita Vaidya <asmita@Asmitas-MacBook-Pro.local>
2017-05-24 14:24:57 -0600

    making UI better

--- src/main/resources/static/js/students.js
+++ src/main/resources/static/js/students.js
@@ -2,37 +2,34 @@ var app = angular.module('studentsApp', []);
 
 app.controller('studentsController', function($scope, $http) {
 
-var getStudents = function() {
-	
-	$http({method: 'GET', url: 'http://localhost:8081/students'})
-	.then(
-		function(response) {
-			$scope.students = response.data;
-			//$scope.studentsService.test = "test2";
-		}, 
-		function(response) {
-			console.log("There was some error!");
-		}
-	);
-}
-
-getStudents();
-
-$scope.insertStudent = function() {
-	var dataToInsert = {name : $scope.name, course : $scope.course};
-	$http({method: 'POST', url: 'http://localhost:8081/students', data: dataToInsert})
+	$scope.getStudents = function() {
+		
+		$http({method: 'GET', url: 'http://localhost:8081/students'})
 		.then(
-			function(data, status, headers, config) {
-				$scope.message = data;
-				getStudents();
-			},
-			function(data, status, headers, config) {
-				alert( "failure message: " + JSON.stringify({data: data}));
+			function(response) {
+				$scope.students = response.data;
+			}, 
+			function(response) {
+				console.log("There was some error!");
 			}
-			
-		);		
-	$scope.name='';
-	$scope.course='';
-}
+		);
+	}
+	
+	$scope.insertStudent = function() {
+		var dataToInsert = {name : $scope.name, course : $scope.course};
+		$http({method: 'POST', url: 'http://localhost:8081/students', data: dataToInsert})
+			.then(
+				function(data, status, headers, config) {
+					$scope.message = data;
+					$scope.getStudents();
+				},
+				function(data, status, headers, config) {
+					alert( "failure message: " + JSON.stringify({data: data}));
+				}
+				
+			);		
+		$scope.name='';
+		$scope.course='';
+	}
 
 });
