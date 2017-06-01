1b431e7a6438d3f4ede270cf973545ac145e6d15
Asmita Vaidya <asmita@Asmitas-MacBook-Pro.local>
2017-05-31 20:07:38 -0600

    Implementing PUT or Edit in angular

--- src/main/resources/static/js/students.js
+++ src/main/resources/static/js/students.js
@@ -1,10 +1,18 @@
 var app = angular.module('studentsApp', []);
 
 app.controller('studentsController', function($scope, $http) {
+	
+	var domainName = "54.245.63.109";
+	//var domainName = "localhost";
+	
+	$scope.init = function() {
+		$scope.editMode = false;
+		$scope.invalidInput = false;
+		$scope.getStudents();
+	}
 
-	$scope.getStudents = function() {
-		
-		$http({method: 'GET', url: 'http://54.245.63.109:8080/students'})
+	$scope.getStudents = function() {	
+		$http({method: 'GET', url: 'http://' + domainName + ':8080/students'})
 		.then(
 			function(response) {
 				$scope.students = response.data;
@@ -16,8 +24,12 @@ app.controller('studentsController', function($scope, $http) {
 	}
 	
 	$scope.insertStudent = function() {
+		if (angular.isUndefined($scope.name) || angular.isUndefined($scope.course) || ($scope.name === "") || ($scope.course === "")) {
+			$scope.invalidInput = true;
+			return;
+		}
 		var dataToInsert = {name : $scope.name, course : $scope.course};
-		$http({method: 'POST', url: 'http://54.245.63.109:8080/students', data: dataToInsert})
+		$http({method: 'POST', url: 'http://' + domainName + ':8080/students', data: dataToInsert})
 			.then(
 				function() {
 					$scope.getStudents();
@@ -32,7 +44,7 @@ app.controller('studentsController', function($scope, $http) {
 	}
 	
 	$scope.deleteStudent = function(id) {
-		$http({method: 'DELETE', url: 'http://54.245.63.109:8080/students/'+id})
+		$http({method: 'DELETE', url: 'http://' + domainName + ':8080/students/'+id})
 			.then(
 				function() {
 						$scope.getStudents();
@@ -42,6 +54,41 @@ app.controller('studentsController', function($scope, $http) {
 					}
 					
 			);
+	}
+	
+	$scope.editStudent = function(id, name, course) {
+		$scope.editMode = true;
+		$scope.id = id;
+		$scope.name = name;
+		$scope.course = course;
+	}
+	
+	$scope.cancelEdit = function() {
+		$scope.init();
+		$scope.name='';
+		$scope.course='';
+	}
+	
+	$scope.updateStudent = function() {
+		debugger
+		if (angular.isUndefined($scope.name) || angular.isUndefined($scope.course) || ($scope.name === "") || ($scope.course === "")) {
+			debugger
+			$scope.invalidInput = true;
+			return;
+		}
+		var dataToEdit = {name : $scope.name, course : $scope.course};
+		$http({method: 'PUT', url: 'http://' + domainName + ':8080/students/'+$scope.id, data: dataToEdit})
+		.then(
+			function() {
+				$scope.init();
+			},
+			function(data) {
+				alert( "failure message: " + JSON.stringify({data: data}));
+			}	
+		
+		);
+		$scope.name='';
+		$scope.course='';
 	}
 
 });
